252c65dc4ae7ea1c45e776d3d848aa5f2d01b14d
Hrishikesh Sathyian <e1121418@u.nus.edu>
2025-07-27 13:12:25 +0800

    Fix loading screen

--- route-jooz-frontend/src/app/route/results/page.tsx
+++ route-jooz-frontend/src/app/route/results/page.tsx
@@ -5,7 +5,17 @@ export const dynamic = "force-dynamic";
 
 export default function RouteResultsPage() {
   return (
-    <Suspense fallback={<p className="p-8 text-orange-600">Loading results...</p>}>
+    <Suspense fallback={
+      <div className="min-h-screen bg-white flex flex-col items-center justify-center">
+      <div className="flex justify-center items-center h-64 text-orange-600">
+          <svg className="animate-spin h-6 w-6 mr-3 text-orange-500" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fill="none" />
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z" />
+          </svg>
+          Loading route data...
+      </div>
+      </div>
+    }>
       <RouteResultsClient />
     </Suspense>
   );
